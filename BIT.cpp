272cca5e58e25fa2f2873431da790845328150c9
IpankajI <pankaj.kumar@tokopedia.com>
2018-11-04 15:57:51 +0530

    range update and range query over BIT

--- BIT.cpp
+++ BIT.cpp
@@ -1,28 +1,67 @@
+// https://www.spoj.com/problems/HORRIBLE/
 #include<bits/stdc++.h>
 typedef long long ll;
 using namespace std;
-const ll N=200005;
-ll BIT[N],n,arr[N];
-void update(ll in,ll value){
-	for( ;in<=n;in+=in&(-in)) BIT[in]+=value;
-}
-ll query(ll id){
-	ll res=0;
-	for(;id>0;id-=id&(-id)) res+=BIT[id];
+const ll N=1e5+1;
+
+ll query(int idx, ll bit[]){
+
+	ll res = 0ll;
+	while(idx >0){
+		res += bit[idx];
+		idx -= idx&(-idx);
+	}
 	return res;
 }
-int main(){
-	int x,q,upto;
-	printf("Enter number of elements (n) and number of queries (q)\n");
-	cin>>n>>q;
-	printf("enter %d integers elements\n",n);
-	for(int i=1;i<=n;i++){
-		scanf("%d",&x);
-		update(i,x);
+
+ll getSum(int idx, ll bit1[], ll bit2[]){
+
+	return query(idx, bit1)*idx - query(idx, bit2);
+}
+
+ll getRangeSum(int p, int q, ll bit1[], ll bit2[]){
+
+	return getSum(q, bit1, bit2) - getSum(p-1, bit1, bit2);
+}
+
+void update(int idx, ll v, int n, ll bit[]){
+	while(idx <= n){
+		bit[idx] += v;
+		idx += idx&(-idx);	
 	}
-	for(int i=q;i--;){
-		cout<<"enter index number to get sum upto( starts at 1)"<<endl;
-		scanf("%d",&upto);
-		printf("sum upto %d elements: %d\n",upto,query(upto));
+}
+
+void updateRange(int p, int q, int v, int n, ll bit1[], ll bit2[]){
+
+	update(p, v, n, bit1);
+	update(q+1, -v, n, bit1);
+
+	update(p, 1ll*v*(p-1), n, bit2);
+	update(q+1, -1ll*q*v, n, bit2);
+}
+
+int main(){
+	ios::sync_with_stdio(0);
+	freopen("../input", "r", stdin);
+	int t;
+	ll bit1[N], bit2[N];
+	cin>>t;
+	while(t--){
+		int n,c;
+		cin>>n>>c;
+		memset(bit1, 0ll, N*sizeof(ll));
+		memset(bit2, 0ll, N*sizeof(ll));
+		while(c--){
+			int one,p,q,v;
+			cin>>one;
+			if(one){
+				cin>>p>>q;
+				cout<<getRangeSum(p,q, bit1, bit2)<<endl;
+			}
+			else{
+				cin>>p>>q>>v;
+				updateRange(p, q, v, n, bit1, bit2);
+			}			
+		}
 	}
 }
